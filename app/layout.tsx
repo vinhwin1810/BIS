db07bb1007634bd1aa90b56950314d0e32361426
Vinh Pham <vinhpham@Vinhs-MacBook-Pro-2.local>
2025-02-13 16:29:42 -0800

    add animation to homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Transition from "./Transition";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Transition>{children}</Transition>
       </body>
     </html>
   );
