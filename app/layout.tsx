d97c859cb7d3b10d5d770439348f1f89765e55d7
Vinh Pham <vinhpham@Vinhs-MacBook-Pro-2.local>
2025-02-13 18:14:45 -0800

    add trail_cursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Transition from "./Transition";
+import CanvasCursor from "./components/cursor/cursor";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Transition>{children}</Transition>
+        <CanvasCursor />
       </body>
     </html>
   );
