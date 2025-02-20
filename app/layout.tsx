7b234c86cb9beeedaae9dc5243d9603106425ce7
Vinh Pham <vinhwin1810@gmail.com>
2025-02-20 10:25:35 -0800

    commented out Cursor for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Transition>{children}</Transition>
-        <CanvasCursor />
+        {/* <CanvasCursor /> */}
       </body>
     </html>
   );
