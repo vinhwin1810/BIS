28ed4bedad632223e9c1851703b13257f1446586
Nvalientenada <nadaaloussi@gmail.com>
2025-02-27 02:32:08 -0500

    Star yellow

--- app/components/Star.tsx
+++ app/components/Star.tsx
@@ -12,7 +12,7 @@ export default function StarButton() {
 
   return (
     <button
-      className="ml-3 bg-[#58E2D3] rounded-full shadow-md p-3"
+      className="ml-3 bg-[#FFC851] rounded-full shadow-md p-3"
       onClick={handleClick}
     >
       <Star className="h-5 w-5" fill={isFilled ? "black" : "none"} />
